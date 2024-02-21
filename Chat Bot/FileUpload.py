58f99245ccdff45bf66f44b3bb5f721a43af8529
carnae <138847713+carnae@users.noreply.github.com>
2024-02-21 21:53:53 +0530

    Update FileUpload.py

--- Chat Bot/FileUpload.py
+++ Chat Bot/FileUpload.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 client = OpenAI()
 
 client.files.create(
-  file=open(r"C:\Users\samar\Visual Studio Projects\json data\revise1.jsonl", "rb"),
+  file=open(r"json data\revise1.jsonl", "rb"),
   purpose="fine-tune"
 )
 
